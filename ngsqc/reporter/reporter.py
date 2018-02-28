11402386d56bb7c1e92d87d24526fe450aa663da
KongDeju <kongdeju@gene.ac>
2018-02-28 10:17:22 +0800

    added

--- ngsqc/reporter/reporter.py
+++ ngsqc/reporter/reporter.py
@@ -1,10 +1,11 @@
-from config import mkdocs,mkdocs_template
+from config import mkdocs
 import os
 from jbiot.logrun import log
 import sys
 
 renderScript = os.path.join(os.path.dirname(os.path.abspath(__file__)),"render.py")
 mdtemplate = os.path.join(os.path.dirname(os.path.abspath(__file__)),"template.md")
+mkdocs_template = os.path.join(os.path.dirname(os.path.abspath(__file__)),"mkdocsTemplate.tgz")
 
 def report(reportDir,prefix):
     
@@ -18,8 +19,8 @@ def report(reportDir,prefix):
 
     # mkdocs build html
     log.info("cp mkdocs template and index template",prefix)
-    target = prefix + ".QC_REPORT"
-    cmd = "cp -r %s %s" % (mkdocs_template,target)
+    target = "mkdocsTemplate"
+    cmd = "tar xvzf %s" % (mkdocs_template)
     log.run(cmd,prefix)
     idx = os.path.join(target,"docs/index.md")
     cmd = "cp %s %s" % (md,idx)
